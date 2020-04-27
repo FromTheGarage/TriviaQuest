06130c70a9dbe3076fc605f383bc0feed311f247
Taj Abiwa <taj.abiwa@yahoo.com>
2020-04-26 20:23:08 -0400

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement; //This namespace allows you to move between scenes
+
+public class SceneLoader : MonoBehaviour
+{
+
+
+    //This method is attached to each button in every scene except the End Game Results Scene. It advances the user to the next Question (Scene)
+    public void loadNextQuestion()
+    {
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //From SceneManager class, run GetActiveScene method, return build index (scene we're currently on)
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+
+
+    //Attached to Play Again button in End Game Results Scene. Returns user to Start Menu
+    public void loadStartScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(0); //Go back to the starting scene (0th index)
+
+
+    }
+
+    
+     
+  
+
+
+
+}
---
+++ Trivia Quest Answer Key.md
@@ -0,0 +1,41 @@
+# Trivia Quest Answer Key
+
+1) How many bones make up the adult human skeleton?
+
+**A: 206**
+
+2) In 1215, King John of England signed this document, limiting the powers of the King of England
+
+**A: The Magna Carta**
+
+3) A group of Tigers is called what?
+
+**A: An ambush**
+
+4) What is the largest city in the world by population?
+
+**A: Tokyo**
+
+5) What is the singular word for "Spaghetti"?
+
+**A: Spaghetto**
+
+6) True or False: Sound travels at a faster speed than light
+
+**A: False**
+
+7) What is the tallest building in the world?
+
+**A: Burj Khalifa**
+
+8) On June 7, 1942, the United States won this decisive battle against the Empire of Japan in the Pacific Theater of World War II
+
+**A: Battle of Midway**
+
+9) True or False: German Chocolate Cake was named after an American baker
+
+**A: True**
+
+10) Who holds the record for the most goals scored in the FIFA World Cup?
+
+**A: Miroslav Klose**
